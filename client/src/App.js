0754f33bda15f3e1d3cda187bc48a62cc069cf8c
youngseo yook <32454317+display1180@users.noreply.github.com>
2023-07-21 11:24:21 +0900

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './common/Header';
+import Main from './common/Main';
+import List from './community/List';
+import Create from './community/Create';
+import GlobalStyle from './GlobalStyle';
+import Detail from './community/Detail';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<Header />
+
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/list' element={<List />} />
+				<Route path='/create' element={<Create />} />
+				<Route path='/detail/:id' element={<Detail />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ client/src/community/List.js
@@ -0,0 +1,30 @@
+import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function List() {
+	const [Posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		axios.get('/api/community/read').then((res) => {
+			console.log(res);
+			setPosts(res.data.communityList);
+		});
+	}, []);
+	return (
+		<Layout name={'List'}>
+			{Posts.map((post) => {
+				return (
+					<article key={post._id}>
+						<h2>
+							<Link to={`/detail/${post.communityNum}`}>{post.title}</Link>
+						</h2>
+					</article>
+				);
+			})}
+		</Layout>
+	);
+}
+
+export default List;
